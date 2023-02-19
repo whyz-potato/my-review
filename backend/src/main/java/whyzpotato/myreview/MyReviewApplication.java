238a67ad37d515abf84338acfbf33b6369f78e8d
Daseul <daseull.rep@gmail.com>
2023-02-19 23:53:10 +0900

    백엔드 셋팅

---
+++ backend/src/main/java/whyzpotato/myreview/MyReviewApplication.java
@@ -0,0 +1,13 @@
+package whyzpotato.myreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReviewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/whyzpotato/myreview/MyReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package whyzpotato.myreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
