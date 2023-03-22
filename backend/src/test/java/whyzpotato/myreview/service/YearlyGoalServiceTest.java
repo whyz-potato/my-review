6cbf6f1bc047cf21fe8af85fb6b7ac832ac23692
Daseul <daseull.rep@gmail.com>
2023-03-22 21:11:55 +0900

    JWT 권한 패스 변경

--- backend/src/test/java/whyzpotato/myreview/service/YearlyGoalServiceTest.java
+++ backend/src/test/java/whyzpotato/myreview/service/YearlyGoalServiceTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import whyzpotato.myreview.domain.*;
+import whyzpotato.myreview.domain.Book;
+import whyzpotato.myreview.domain.Review;
+import whyzpotato.myreview.domain.ReviewStatus;
+import whyzpotato.myreview.domain.Users;
+import whyzpotato.myreview.repository.ItemRepository;
 import whyzpotato.myreview.repository.UsersRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -18,7 +21,7 @@ public class YearlyGoalServiceTest {
     @Autowired
     private UsersRepository usersRepository;
     @Autowired
-    private ItemService itemService;
+    private ItemRepository itemRepository;
     @Autowired
     private ReviewService reviewService;
     @Autowired
@@ -35,18 +38,18 @@ public class YearlyGoalServiceTest {
         Book book4 = Book.builder().title("책4").releaseDate(LocalDate.of(2012, 8, 4)).author("au").description("---").image("img").isbn("7814").build();
         Book book5 = Book.builder().title("책5").releaseDate(LocalDate.of(2012, 8, 5)).author("au").description("---").image("img").isbn("7815").build();
         Book book6 = Book.builder().title("책6").releaseDate(LocalDate.of(2012, 8, 6)).author("au").description("---").image("img").isbn("7816").build();
-        itemService.save(book1);
-        itemService.save(book2);
-        itemService.save(book3);
-        itemService.save(book4);
-        itemService.save(book5);
-        itemService.save(book6);
-        reviewService.save((Review.builder().users(users).item(book1).date(LocalDate.of(2021,1,1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음1").build()));
-        reviewService.save((Review.builder().users(users).item(book2).date(LocalDate.of(2022,2,1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음2").build()));
-        reviewService.save((Review.builder().users(users).item(book3).date(LocalDate.of(2022,3,1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음3").build()));
-        reviewService.save((Review.builder().users(users).item(book4).date(LocalDate.of(2022,4,1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음4").build()));
-        reviewService.save((Review.builder().users(users).item(book5).date(LocalDate.of(2023,5,1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음5").build()));
-        reviewService.save((Review.builder().users(users).item(book6).date(LocalDate.of(2023,6,1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음6").build()));
+        itemRepository.save(book1);
+        itemRepository.save(book2);
+        itemRepository.save(book3);
+        itemRepository.save(book4);
+        itemRepository.save(book5);
+        itemRepository.save(book6);
+        reviewService.save((Review.builder().users(users).item(book1).date(LocalDate.of(2021, 1, 1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음1").build()));
+        reviewService.save((Review.builder().users(users).item(book2).date(LocalDate.of(2022, 2, 1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음2").build()));
+        reviewService.save((Review.builder().users(users).item(book3).date(LocalDate.of(2022, 3, 1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음3").build()));
+        reviewService.save((Review.builder().users(users).item(book4).date(LocalDate.of(2022, 4, 1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음4").build()));
+        reviewService.save((Review.builder().users(users).item(book5).date(LocalDate.of(2023, 5, 1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음5").build()));
+        reviewService.save((Review.builder().users(users).item(book6).date(LocalDate.of(2023, 6, 1)).status(ReviewStatus.DONE).rate(4).content("보고있는데 재밌음6").build()));
         //when
         yearlyGoalService.historyYearlyGoal(users.getId());
     }
